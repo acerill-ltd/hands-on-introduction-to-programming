e8914b4b45faea3fe75d5fd3b8247a6a13f70bd4
Francois Aubertin <francois@acerill.com>
2022-05-30 20:38:20 +0100

    Adding fauna client

--- src/lambda/server.js
+++ src/lambda/server.js
@@ -1,4 +1,10 @@
+const faunadb = require('faunadb');
 
 export async function handler(event, context) {
-}
+  const client = new faunadb.Client({secret: process.env.db_password});
 
+  return {
+    statusCode: 200,
+    body: JSON.stringify({})
+  };
+}
